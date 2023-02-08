e45c7a673b9b02ec84a6cb21474accb81a4c82b3
vikaass_08 <vikas.pathak@juspay.in>
2023-02-08 18:24:14 +0530

    unused changes

--- frontend/src/components/SiteContext.jsx
+++ frontend/src/components/SiteContext.jsx
@@ -4,10 +4,10 @@ import React, { useState, createContext } from "react";
 export const SiteContext = createContext();
 
 export const SiteProvider = (props) => {
-  const [auth, setAuth] = useState(localStorage.getItem("auth")? localStorage.getItem("auth") :null);
-  const [dp, setDp] = useState(localStorage.getItem("dp")? localStorage.getItem("dp") :null);
+  const [auth, setAuth] = useState(localStorage.getItem("auth")? localStorage.getItem("auth") : null);
+  const [dp, setDp] = useState(localStorage.getItem("dp")? localStorage.getItem("dp") : null);
   return (
-    <SiteContext.Provider value={[auth, setAuth],[dp, setDp]}>
+    <SiteContext.Provider value={[[auth, setAuth],[dp, setDp]]}>
       {props.children}
     </SiteContext.Provider>
   );
